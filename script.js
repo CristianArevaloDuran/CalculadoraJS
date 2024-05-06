598c59706cae27b18334f105aa60f11ba6afe732
Cristian Arevalo <cristianarevaloduran@gmail.com>
2024-05-05 22:22:01 -0500

    Beautify

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let random = Math.floor(Math.random() * 100) + 1;
+function loadPokemon() {
+    let random = Math.floor(Math.random() * 1000) + 1;
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
-    .then(response => response.json())
-    .then(data => {
-        fetch(data.forms[0].url)
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('pokemon').src = data.sprites.front_default;
-                document.getElementById('pName').innerText = data.name;
-                document.getElementById('pageIco').href = data.sprites.front_default;
-            });
-    });
+    fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+        .then(response => response.json())
+        .then(data => {
+            fetch(data.forms[0].url)
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById('pokemon').src = data.sprites.front_default;
+                    document.getElementById('pokemon').title = data.name;
+                    document.getElementById('pName').innerText = data.name;
+                    document.getElementById('pName').title = data.name;
+                    document.getElementById('pageIco').href = data.sprites.front_default;
+                });
+        })
+        .catch(err => console.log(err));
+}
+
+loadPokemon();
+
+
+document.querySelector('.header').addEventListener('click', loadNewPokemon);
+
+function loadNewPokemon() {
+    document.getElementById('pokemon').src = 'ZKZg.gif';
+    document.getElementById('pName').innerText = 'Loading...';
+    loadPokemon();
+}
